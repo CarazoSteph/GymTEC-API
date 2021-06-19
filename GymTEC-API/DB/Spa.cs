d8453cb2b7fedf31fca5d05cba9a384828b07a9f
AngelCurls <stefany_199829@hotmail.com>
2021-06-18 21:56:44 -0600

    getters and setters for class spa

--- GymTEC-API/DB/Spa.cs
+++ GymTEC-API/DB/Spa.cs
@@ -2,6 +2,44 @@
 {
     public class Spa
     {
-        
+        //atributtes
+        public string masajeRelajante;
+        public string masajeDescargaMuscular;
+        public string sauna;
+        public string banosVapor;
+
+        //constructor
+        public Spa(string masajeRelajante, string masajeDescargaMuscular, string sauna, string banosVapor)
+        {
+            this.masajeRelajante = masajeRelajante;
+            this.masajeDescargaMuscular = masajeDescargaMuscular;
+            this.sauna = sauna;
+            this.banosVapor = banosVapor;
+        }
+
+        //getters and setters
+        public string MasajeRelajante
+        {
+            get => masajeRelajante;
+            set => masajeRelajante = value;
+        }
+
+        public string MasajeDescargaMuscular
+        {
+            get => masajeDescargaMuscular;
+            set => masajeDescargaMuscular = value;
+        }
+
+        public string Sauna
+        {
+            get => sauna;
+            set => sauna = value;
+        }
+
+        public string BanosVapor
+        {
+            get => banosVapor;
+            set => banosVapor = value;
+        }
     }
 }
