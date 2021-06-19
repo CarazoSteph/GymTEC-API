b6be1a3d07168e691024ec74e92393143efc5e0e
AngelCurls <stefany_199829@hotmail.com>
2021-06-18 22:30:31 -0600

    getters and setters for puesto and tipo planilla class

--- GymTEC-API/DB/Puesto.cs
+++ GymTEC-API/DB/Puesto.cs
@@ -2,6 +2,44 @@
 {
     public class Puesto
     {
-        
+        //atributtes
+        public string administrador;
+        public string instructor;
+        public string dependienteSpa;
+        public string dependienteTienda;
+
+        //contructor
+        public Puesto(string administrador, string instructor, string dependienteSpa, string dependienteTienda)
+        {
+            this.administrador = administrador;
+            this.instructor = instructor;
+            this.dependienteSpa = dependienteSpa;
+            this.dependienteTienda = dependienteTienda;
+        }
+
+        //getters and setters
+        public string Administrador
+        {
+            get => administrador;
+            set => administrador = value;
+        }
+
+        public string Instructor
+        {
+            get => instructor;
+            set => instructor = value;
+        }
+
+        public string DependienteSpa
+        {
+            get => dependienteSpa;
+            set => dependienteSpa = value;
+        }
+
+        public string DependienteTienda
+        {
+            get => dependienteTienda;
+            set => dependienteTienda = value;
+        }
     }
 }
--- GymTEC-API/DB/TipoPlanilla.cs
+++ GymTEC-API/DB/TipoPlanilla.cs
@@ -2,6 +2,36 @@
 {
     public class TipoPlanilla
     {
-        
+        //atributtes
+        public string pagoMensual;
+        public string pagoXhora;
+        public string pagoXclase;
+
+        //constructor
+        public TipoPlanilla(string pagoMensual, string pagoXhora, string pagoXclase)
+        {
+            this.pagoMensual = pagoMensual;
+            this.pagoXhora = pagoXhora;
+            this.pagoXclase = pagoXclase;
+        }
+
+        //getters snd setters
+        public string PagoMensual
+        {
+            get => pagoMensual;
+            set => pagoMensual = value;
+        }
+
+        public string PagoXhora
+        {
+            get => pagoXhora;
+            set => pagoXhora = value;
+        }
+
+        public string PagoXclase
+        {
+            get => pagoXclase;
+            set => pagoXclase = value;
+        }
     }
 }
