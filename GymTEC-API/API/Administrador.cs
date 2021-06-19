5df2a3e59aa847ac95f5fa8976191ddd7323b478
AngelCurls <stefany_199829@hotmail.com>
2021-06-18 23:47:20 -0600

    funcion para verificar log in

--- GymTEC-API/API/Administrador.cs
+++ GymTEC-API/API/Administrador.cs
@@ -2,6 +2,26 @@
 {
     public class Administrador
     {
-        
+        /*// Funcion que verifica el log in proveniente de la pagina web
+        // Entrada: un string con el correo y otro con la contrasena
+        // Salida: un string del tipo de usuario que ingresa
+        // Restricciones: las entradas deben ser strings
+        public static string login(string loginCorreo, string loginContrasena)
+        {
+            if (loginCorreo.Equals("admin") && loginContrasena.Equals("123"))
+            {
+                return "admin";
+            }
+            for (int i = 0; i < lista_Usuarios.Count; i++)
+            {
+                if (lista_Usuarios[i].correo.Equals(loginCorreo) &&
+                    lista_Usuarios[i].Contrasena.Equals(loginContrasena))
+                {
+                    return "usuario";
+                }
+            }
+            
+            return "denegar";
+        }*/
     }
 }
