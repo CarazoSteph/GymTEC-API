077cd12fdf26975c345782856e015600bed72313
JATS14 <josetrejos2000@gmail.com>
2021-06-20 14:44:45 -0600

    Clase log in Register

---
+++ GymTEC-API/Controllers/LogInRegisterController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using GymTEC_API.DB;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+namespace GymTEC_API.Controllers
+{
+    //funcion encargada del registro y de logeo de los usuarios
+    [ApiController]
+    [Route("[controller]")]
+    public class LogInRegisterController : ControllerBase
+    {
+        // funcion encargada de verificar los datos de log in de un usuario
+        //Entrada: los datos en forma de loginEntrada de correo y contraseña
+        //Salida: la respuesta de si es admitido o no, y si es usuario o administrador
+        //Restricciones: no posee restricciones
+        [HttpPost]
+        [Route("verificar")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public respuesta Verificar_Login(LoginEntrada login)
+        {
+            //Proceso de Log In
+            if (Administrador.login(login.correo, login.contrasena) == "admin")
+            {
+                return new respuesta("admin");
+            }
+            if (Administrador.login(login.correo, login.contrasena) == "usuario")
+            {
+                return new respuesta("usuario");
+            }
+            return new respuesta("denegar");
+        }
+        
+        [HttpPost]
+        [Route("registrar")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public respuesta Verificar_registro(Usuario nuevoUsuario)
+        {
+            string jsonString = JsonSerializer.Serialize(nuevoUsuario);
+            Console.WriteLine("Lo que llega de registrarse: "+ jsonString);
+
+            Administrador.registrarUsuario(nuevoUsuario);
+            return new respuesta("ok");
+        }
+        
+        
+        
+    }
+}
---
+++ GymTEC-API/Controllers/respuesta.cs
@@ -0,0 +1,44 @@
+﻿namespace GymTEC_API.Controllers
+{
+    public class respuesta
+    {
+        public string status;
+        public string respuestaS;
+
+        //Contructor los datos de respuesta no son siempre necesarios
+        public respuesta(string status)
+        {
+            this.status = status;
+        }
+        //Getters y setters
+        public string Status
+        {
+            get => status;
+            set => status = value;
+        }
+
+        public string RespuestaS
+        {
+            get => respuestaS;
+            set => respuestaS = value;
+        }
+    }
+    
+    //Plantilla de datos ingresados por el usuario desde la web app
+    public class LoginEntrada
+    {
+        public string correo;
+        public string contrasena;
+
+        public LoginEntrada(string correo, string contrasena)
+        {
+            this.correo = correo;
+            this.contrasena = contrasena;
+        }
+
+        public string Correo => correo;
+
+        public string Contrasena => contrasena;
+    }
+
+}
